de090724a290868707bb9ad961cfebee70cc18af
Jefferson Cantores <jeffersoncantores13@gmail.com>
2023-02-22 13:55:37 +0800

    Updated file

--- SampleFramework/src/test/java/com/sampleframework/testrunner/Runner.java
+++ SampleFramework/src/test/java/com/sampleframework/testrunner/Runner.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = "Features",
